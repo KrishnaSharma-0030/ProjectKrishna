5381d84fd229b04a88a2cf2c4e36c4f0c4f3b908
KrishnaSharma-0030 <shar1601@sheridancollege.ca>
2026-06-01 13:00:15 -0400

    Initial NetBeans project setup

---
+++ src/main/java/com/mycompany/projectkrishna/ProjectKrishna.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectkrishna;
+
+/**
+ *
+ * @author ks200
+ */
+public class ProjectKrishna {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
