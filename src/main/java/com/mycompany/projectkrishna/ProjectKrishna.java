1443df9c5c5d4a83587601bb9039362a484544cc
KrishnaSharma-0030 <shar1601@sheridancollege.ca>
2026-06-01 13:05:12 -0400

    Add simple calculator application

--- src/main/java/com/mycompany/projectkrishna/ProjectKrishna.java
+++ src/main/java/com/mycompany/projectkrishna/ProjectKrishna.java
@@ -11,6 +11,13 @@ package com.mycompany.projectkrishna;
 public class ProjectKrishna {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1 = 20;
+        int num2 = 5;
+
+        System.out.println("Simple Calculator");
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
     }
 }
